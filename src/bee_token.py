410395897cf03ea3c076c3605674f5e0e21ba93d
דור שפירא <Dor.Shapira@outlook.com>
2023-04-21 10:52:07 +0300

    refactored lexer

--- src/bee_token.py
+++ src/bee_token.py
@@ -1,19 +1,20 @@
 import enum
 
 
-class TokenKind(enum.IntEnum):
-    Plus = enum.auto()  # +
-    Minus = enum.auto()  # -
-    Asterisk = enum.auto()  # *
-    ForwardSlash = enum.auto()  # /
-    Integer = enum.auto()
-    Float = enum.auto()
+class TokenKind(enum.Enum):
+    ADD_OP = enum.auto()  # +
+    MIN_OP = enum.auto()  # -
+    MUL_OP = enum.auto()  # *
+    DIV_OP = enum.auto()  # /
+
     OpenParenthesis = enum.auto()  # (
     ClosingParenthesis = enum.auto()  # )
     Comma = enum.auto()  # ,
-    Identifier = enum.auto()  # ,
-    END = enum.auto()  # end token
 
+    Integer = enum.auto()
+    Float = enum.auto()
+    Identifier = enum.auto()
+    END = enum.auto()  # end tokens string token
 
 
 class Token:
