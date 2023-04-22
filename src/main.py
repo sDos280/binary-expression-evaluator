e914a723ad6ffff8f4156a6c0f7800c12eb87655
דור שפירא <Dor.Shapira@outlook.com>
2023-04-22 09:36:15 +0300

    change input string

--- src/main.py
+++ src/main.py
@@ -2,8 +2,7 @@ import src.bee_lexer
 import src.bee_parser
 
 if __name__ == '__main__':
-    """input_string: str = "6 + 3 - 8.236 / cos(5 + 6 * 3.2101)"""
-    input_string: str = "sin(sin(3363))"
+    input_string: str = "6 + 3 - 8.236 / cos(5 + 6 * 3.2101)"
 
     lexer: src.bee_lexer.Lexer = src.bee_lexer.Lexer(input_string)
 
